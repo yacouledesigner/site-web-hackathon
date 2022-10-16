b1fca6c1087f8619a5f69f8d47909afdaf2460db
yacouledesigner <yacouledesigner@gmail.com>
2022-10-16 20:05:50 +0000

    hackathon-vrai

--- src/components/Courses-section/Courses.jsx
+++ src/components/Courses-section/Courses.jsx
@@ -2,18 +2,20 @@ import { Container, Row, Col } from "reactstrap";
 import code from "../../assests/images/code.gif";
 // import css from "../../assests/images/css.png";
 import jeux from "../../assests/images/jeux.png";
+import cours1 from "../../assests/video/cours1.mp4";
+import Iframe from 'react-iframe'
 import "./courses.css";
 // import CourseCard from "./CourseCard";
 
 const coursesData = [
-  // {
-  //   id: "01",
-  //   title: "HTML pour les enfants",
-  //   lesson: 12,
-  //   students: 12.5,
-  //   rating: 5.9,
-  //   imgUrl: html,
-  // },
+  {
+    id: "01",
+    title: "HTML pour les enfants",
+    lesson: 12,
+    students: 12.5,
+    rating: 5.9,
+    imgUrl: cours1,
+  },
 
   {
     id: "02",
@@ -40,17 +42,19 @@ const Courses = () => {
             </div>
           </Col>
           {/* {coursesData.map((item) => ( */}
-          <Col lg="12" md="6" sm="12" className='d-flex flex-column justify-content-center align-items-center rounded' style={{backgroundColor:'#00CCCC',height:500}}>
+          <Col lg="5" md="6" sm="12" className='d-flex flex-column justify-content-center align-items-center rounded' style={{backgroundColor:'#00CCCC',height:500}}>
               {/* <CourseCard key={item.id} item={item} /> */}
               <div>
                 <img src={jeux} alt="" />
               </div>
               <h2>EliteCoding</h2>
               <p>Explore le code grâce au foot</p>
-              <button id="btn1">Jouer contre l'ordi</button>
-              <button id="btn2">Comment jouer ?</button>
+              <a href="http://192.168.1.115:4200/home" target='_blank' class="btn btn-warning">Démarrer</a>
           </Col>
-          {/* ))} */}
+          <Col lg="5" md="6" sm="12">
+            <video className="shadow-sm" style={{height:500,width:700,marginLeft:30, borderRadius:20, border:'1px solid black'}} src={cours1} controls></video>
+            <legend className="h1" style={{marginLeft:250}}>Vidéo de cours</legend>
+          </Col>
         </Row>
       </Container>
     </section>
